4998f4c01ceeba19002acecdcff2e1e17d871c28
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-22 07:49:04 +0530

    create migration table

--- database/migrations/2018_11_22_012743_create_products_table.php
+++ database/migrations/2018_11_22_012743_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('image');
+            $table->bigInteger('price');
+            $table->text('description');
             $table->timestamps();
         });
     }
