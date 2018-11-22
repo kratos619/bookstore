b92a71e9db5290a334a2fbe5deed9fedac93a0a2
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-22 07:07:32 +0530

    genrate model controller and migration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', 'ProductsController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
