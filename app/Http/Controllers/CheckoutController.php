11c3a604c753adead568e89ee2850f932cd6eba5
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-23 11:49:41 +0530

    Create payment Charge

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Cart;
 use App\Product;
+use Stripe\Stripe;
+use Stripe\Charge;
 
 class CheckoutController extends Controller
 {
@@ -15,6 +17,19 @@ class CheckoutController extends Controller
 
     public function pay()
     {
-        dd(request()->all());
+        //  dd(request()->all());
+
+        Stripe::setApiKey("sk_test_V4tIgxkmQFL8zflHYTbwtg0p");
+
+//        $token = request()->stripeToken;
+        $charge = Charge::create([
+            'amount' => Cart::total() * 100,
+            'currency' => 'usd',
+            'description' =>  'My Book Store ',
+            'source' =>request()->stripeToken
+        ]);
+
+        dd('yment success');
+        //dd($charge);
     }
 }
