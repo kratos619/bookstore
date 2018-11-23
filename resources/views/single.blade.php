9134f8ce28cd5f90f5cf710ca1989c65be7365ff
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-23 09:41:58 +0530

    Fix main js files

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -27,10 +27,10 @@
                     <form action="{{route('cart.add')}}" method="POST">
                         {{ csrf_field() }}
                         <div class="quantity">
-                        <a href="#" class="quantity-minus">-</a>
+                        <a href="#" class="quantity-minus quantity-minus-d">-</a>
                         <input title="Qty" name="qty" class="email input-text qty text" type="text" value="2">
                         
-                        <a href="#" class="quantity-plus">+</a>
+                        <a href="#" class="quantity-plus quantity-plus-d">+</a>
                     </div>
                 <input type="hidden" name="pdt_id" value="{{$product->id}}">
 
